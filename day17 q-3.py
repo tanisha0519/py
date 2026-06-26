e342c33f606433aab1b52fa80a3b84513aefa36e
tanisha0519 <tanishas1910@gmail.com>
2026-06-26 23:27:15 +0530

    Add files via upload

---
+++ day17 q-3.py
@@ -0,0 +1,15 @@
+n1=int(input("Enter the number of element:"))
+arr1=[]
+for i in range(n1):
+    arr1.append(int(input("enter the element:")))
+n2 =int(input("Enter the number of element:"))
+arr2=[]
+for i in range(n2):
+        arr2.append(int(input("Enter the element:")))
+intersection=[]
+for i in arr1:
+      if i in arr2 and i not in intersection :
+            intersection.append(i)
+print("Print intersection of arrays:")
+for i in intersection:
+      print(i,end="")
