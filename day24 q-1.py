4cff1560dc65a6668ce19af8f0f9b86d8caec0c0
tanisha0519 <tanishas1910@gmail.com>
2026-06-27 23:23:19 +0530

    Add files via upload

---
+++ day24 q-1.py
@@ -0,0 +1,7 @@
+str1=input("Enter first string:")
+str2=input("Enter second string:")
+if len(str1)==len(str2)and str2 in (str1+str1):
+    print("Strings are rotations of each other")
+else:
+    print("Strings are not rotation of eachother")
+    
